cd57a15b228636f2a1097d19d04baa843b0d0054
liancheng <wpshlln@163.com>
2026-09-09 14:42:07 +0800

    feat: add Sift browser tab favicon

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ const mimeTypes = {
   ".html": "text/html; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
-  ".mjs": "text/javascript; charset=utf-8"
+  ".mjs": "text/javascript; charset=utf-8",
+  ".svg": "image/svg+xml"
 };
 
 function json(response, status, data) {
